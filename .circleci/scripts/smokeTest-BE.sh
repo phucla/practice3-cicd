5863480da41c8a7783c61bd3bedf79cce6e3987a
PhucLa <laducphuc@gmail.com>
2023-05-01 15:23:07 +0700

    Update config

--- .circleci/scripts/smokeTest-BE.sh
+++ .circleci/scripts/smokeTest-BE.sh
@@ -5,7 +5,7 @@ BACKEND_IP=$(tail -n 1 .circleci/ansible/inventory.txt)
   echo "${API_URL}"
   if curl --connect-timeout 10 "${API_URL}/api/status" | grep "ok"
   then
-  	exit 0
+  	exit 1
   else
   	exit 1
   fi
